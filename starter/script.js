935376192c526db1abf10b080d07e89ebcc76c96
hanipnk <hanipnk@gmail.com>
2021-07-04 21:05:00 -0400

    AJAX CALL

--- starter/script.js
+++ starter/script.js
@@ -4,3 +4,43 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (contry) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${contry}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+
+    const [data1, data2] = JSON.parse(this.responseText);
+    let data;
+    if (data2) {
+      data = data2;
+    } else {
+      data = data1;
+    }
+    //console.log(data);
+
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('korea');
+getCountryData('portugal');
+getCountryData('usa');
