1e3b5c217682dd23f206e2be4960532f9fc4da9c
hanipnk <hanipnk@gmail.com>
2021-07-12 15:17:11 -0400

    Coding challenge3

--- starter/script.js
+++ starter/script.js
@@ -689,6 +689,7 @@ Promise.any([
 
   */
 
+/*  
 // Coding Challenge 3
 const imgContainer = document.querySelector('.images');
 
@@ -774,6 +775,68 @@ const loadAll = async function (imgArr) {
     const imgsEl = await Promise.all(imgs);
     console.log(imgsEl);
     imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge 3
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image load is failed'));
+    });
+  });
+};
+
+// Part 1
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+// loadNPause();
+
+//Part 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+    const imgEl = await Promise.all(imgs);
+    console.log(imgEl);
+    imgEl.forEach(img => img.classList.add('parallel'));
   } catch (err) {
     console.error(err);
   }
